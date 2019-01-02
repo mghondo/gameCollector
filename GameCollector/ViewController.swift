92c4e3b73dc6eb6aa8779df66b94e3cb8e294a86
Morgan Hondros <morganhondros@gmail.com>
2019-01-02 16:32:07 -0500

    1st Commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Morgan Hondros on 1/2/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
